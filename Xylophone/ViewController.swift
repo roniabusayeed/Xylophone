7fdb30ac417bbcfc9402884072b369a3a94b32f4
Abu Sayeed Roni <roni.abusayeed@dal.ca>
2023-05-06 19:45:05 -0300

    Implment sound playback

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let soundTitle = sender.currentTitle!
+        playSound(for: soundTitle)
+    }
     
-
+    func playSound(for soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
 }
 
